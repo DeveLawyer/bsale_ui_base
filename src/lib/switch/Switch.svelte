12e80a96df72ac0c3bb0b0da96137e7312d2d8e2
Zamir Narro <josue.narro.a@gmail.com>
2022-07-23 13:42:02 -0500

    Remove switch icons

--- src/lib/switch/Switch.svelte
+++ src/lib/switch/Switch.svelte
@@ -1,10 +1,10 @@
 <script>
-  import { onMount } from "svelte"
-  import { MDCSwitch } from "@material/switch"
+  import { onMount } from "svelte";
+  import { MDCSwitch } from "@material/switch";
 
   onMount(() => {
-    const switchControl = new MDCSwitch(document.querySelector(".mdc-switch"))
-  })
+    const switchControl = new MDCSwitch(document.querySelector(".mdc-switch"));
+  });
 </script>
 
 <div>
@@ -13,7 +13,8 @@
     class="mdc-switch mdc-switch--unselected"
     type="button"
     role="switch"
-    aria-checked="false">
+    aria-checked="false"
+  >
     <div class="mdc-switch__track" />
     <div class="mdc-switch__handle-track">
       <div class="mdc-switch__handle">
@@ -21,19 +22,6 @@
           <div class="mdc-elevation-overlay" />
         </div>
         <div class="mdc-switch__ripple" />
-        <div class="mdc-switch__icons">
-          <svg
-            class="mdc-switch__icon mdc-switch__icon--on"
-            viewBox="0 0 24 24">
-            <path
-              d="M19.69,5.23L8.96,15.96l-4.23-4.23L2.96,13.5l6,6L21.46,7L19.69,5.23z" />
-          </svg>
-          <svg
-            class="mdc-switch__icon mdc-switch__icon--off"
-            viewBox="0 0 24 24">
-            <path d="M20 13H4v-2h16v2z" />
-          </svg>
-        </div>
       </div>
     </div>
   </button>
